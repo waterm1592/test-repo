a279a6056bbac7378a6a2b7093fb3db0fd09a56c
ksm2eond1592 <163498465+ksm2eond1592@users.noreply.github.com>
2024-03-24 21:27:16 +0900

    Update Test.c

--- Test.c
+++ Test.c
@@ -7,7 +7,7 @@ int main() {
   int StudentNum = 202302762;
   int age = 21
   printf("Hello Wolrd!");
-  printf("My name is Kim Soo Min");
+  printf("My name is Kim Soo Min1");
   printf("My student number is %d", StudentNum);
   printf("My age is %d", age);
   printf("Hello Wolrd!");
