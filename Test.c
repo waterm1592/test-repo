17c9091446229a429034ca24f3e632f9b38318b9
waterm1592 <163493938+waterm1592@users.noreply.github.com>
2024-03-24 21:09:56 +0900

    Update Test.c

--- Test.c
+++ Test.c
@@ -4,6 +4,8 @@ touched by ksm2eond1592
 #include <stdio.h>
 
 int main() {
+  int i = 202302762;
   printf("Hello Wolrd!");
   printf("My name is Kim Soo Min5");
+  printf("My student number is %d", i);
 }
